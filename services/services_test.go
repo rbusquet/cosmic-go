8bd2f95d9c60bccfb6134c2ec0fb9d12ade5a9db
Ricardo Busquet <7198302+rbusquet@users.noreply.github.com>
2022-02-14 22:50:18 -0500

    move model service tests to service layer

--- services/services_test.go
+++ services/services_test.go
@@ -46,6 +46,59 @@ func (suite *ServicesSuite) TestSaves() {
 	assert.Equal(suite.T(), true, repo.Saved)
 }
 
+func (suite *ServicesSuite) TestPrefersWarehouseStockBatchesToShipments() {
+	tomorrow := time.Now().AddDate(0, 0, 1)
+	inStockBatch := model.NewBatch("in-stock-batch", "RETRO-CLOCK", 100, time.Time{})
+	shipmentBatch := model.NewBatch("shipment-batch", "RETRO-CLOCK", 100, tomorrow)
+	repo := repository.NewFakeRepository(inStockBatch, shipmentBatch)
+
+	line := model.OrderLine{OrderID: "oref", SKU: "RETRO-CLOCK", Quantity: 10}
+
+	services.Allocate(line, repo)
+	assert.Equal(suite.T(), 90, inStockBatch.AvailableQuantity())
+	assert.Equal(suite.T(), 100, shipmentBatch.AvailableQuantity())
+}
+
+func (suite *ServicesSuite) TestPrefersEarlierBatches() {
+	today := time.Now()
+	tomorrow := today.AddDate(0, 0, 1)
+	later := today.AddDate(0, 1, 0)
+	earliest := model.NewBatch("speedy-batch", "MINIMALIST-SPOON", 100, today)
+	medium := model.NewBatch("normal-batch", "MINIMALIST-SPOON", 100, tomorrow)
+	latest := model.NewBatch("slow-batch", "MINIMALIST-SPOON", 100, later)
+	repo := repository.NewFakeRepository(earliest, medium, latest)
+	line := model.OrderLine{OrderID: "order1", SKU: "MINIMALIST-SPOON", Quantity: 10}
+
+	services.Allocate(line, repo)
+
+	assert.Equal(suite.T(), 90, earliest.AvailableQuantity())
+	assert.Equal(suite.T(), 100, medium.AvailableQuantity())
+	assert.Equal(suite.T(), 100, latest.AvailableQuantity())
+}
+
+func (suite *ServicesSuite) TestReturnsAllocatedBatchRef() {
+	tomorrow := time.Now().AddDate(0, 0, 1)
+	inStockBatch := model.NewBatch("in-stock-batch", "HIGHBROW-POSTER", 100, time.Time{})
+	shipmentBatch := model.NewBatch("shipment-batch", "HIGHBROW-POSTER", 100, tomorrow)
+	repo := repository.NewFakeRepository(inStockBatch, shipmentBatch)
+
+	line := model.OrderLine{OrderID: "oref", SKU: "HIGHBROW-POSTER", Quantity: 10}
+
+	allocation, err := services.Allocate(line, repo)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), inStockBatch.Reference, allocation)
+}
+
+func (suite *ServicesSuite) TestRaisesOutOfStockExceptionIfCannotAllocate() {
+	batch := model.NewBatch("batch1", "SMALL-FORK", 10, time.Now())
+	line := model.OrderLine{OrderID: "order1", SKU: "SMALL-FORK", Quantity: 10}
+	_, err := model.Allocate(line, batch)
+	assert.NoError(suite.T(), err)
+	line2 := model.OrderLine{OrderID: "order2", SKU: "SMALL-FORK", Quantity: 10}
+	_, err = model.Allocate(line2, batch)
+	assert.EqualError(suite.T(), err, "Out of stock for SKU SMALL-FORK")
+}
+
 func TestServicesSuite(t *testing.T) {
 	suite.Run(t, new(ServicesSuite))
 }
